b7d5fb45fbec3db4dae72cc5f0e75f918cd54823
nehanapkhan <nehanapkhan2k@gmail.com>
2025-02-01 10:38:09 -0500

    coment

---
+++ lesson1/coment.py
@@ -0,0 +1,20 @@
+#assinging different varibles
+name="penguinn"
+age =12
+is_student= True
+weight= 12.34
+print("name:",name)
+print("data type of name is",type(name))
+print("age:",age)
+print("data type of age is",type(age))
+print("is_student:",is_student)
+print("data type of weight is",type(is_student))
+print("weight:",weight)
+print("data type of weight is",type(weight))
+print("\n after type castng....")
+age = str(age)
+print(age)
+print("data type of age is",type(age))
+weight = int(weight)
+print(weight)
+print("data type of weight is",type(weight))
