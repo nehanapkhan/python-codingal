45b1fb2ae4a777e10c42fec0b210c49f3c2f8530
nehanapkhan <nehanapkhan2k@gmail.com>
2025-02-01 10:17:54 -0500

    data type

---
+++ lesson1/data type.py
@@ -0,0 +1,11 @@
+#lets check the dataype of different values
+
+a=5
+print("type of is",type(a))
+
+b=2.5
+print("type of b is",type (b) )
+c-"coding"
+print("type of c is",type(c))
+d-True
+print("type of d is",type(d))
